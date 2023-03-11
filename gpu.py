9fd2aa3da533208951641ec5a65ec80e1fb94dfd
marwa-mohamed-dev <47494409+marwa-mohamed-dev@users.noreply.github.com>
2023-03-11 20:26:45 +0100

    a

--- gpu.py
+++ gpu.py
@@ -1,9 +1,9 @@
 from numba import cuda
 import brute_force as b
-
+import hashlib
 
 @cuda.jit
-def kernel_combinaisons(mdp, type_hash):
+def kernel_combinaisons(mdp, hash):
     all = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','@','[',']','^','_','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','{','}','<','>','=','|','~','?']
     tx = cuda.threadIdx.x
     bx = cuda.blockIdx.x
@@ -12,6 +12,12 @@ def kernel_combinaisons(mdp, type_hash):
     #taille maximale que fait notre mdp
     MAX_LENGTH = 15
     
-    if i < mdp.size: #Les différentes tailles de mdp à trouver
-        b.combinaisons(mdp, 0, i, '', all, type_hash)
-        print("Tous les mots de passe de", i, "caractères ont été testé.")
+    if i < MAX_LENGTH: #Les différentes tailles de mdp à trouver
+        for i in range(len(all)):
+            mdp_test += all[i]
+            if(hashlib.sha1(mdp_test.encode()).hexdigest() == hash) :
+                print("Le mot de passe est : ", mdp_test)
+                return mdp_test
+
+
+        kernel_combinaisons( mdp_test,  hash) #Recursion avec caractère suivant)
--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import hashlib
 from brute_force import bf
 import gpu
 from numba import cuda
+import numpy
 # import tensorflow as tf
-print(hashlib.algorithms_available)
-HASH = ''
-mdp = 'a9993e364706816aba3e25717850c26c9cd0d89d'
+
+
+HASH = 'a9993e364706816aba3e25717850c26c9cd0d89d'
+mdp = ''
 d_HASH = cuda.to_device(HASH)
 d_mdp = cuda.to_device(mdp)
 gpu.kernel_combinaisons[5,5](d_mdp, d_HASH)
+mdp = numpy.empty(shape=d_mdp.shape, dtype=d_mdp.dtype)
+d_mdp.copy_to_host(mdp)
+print(mdp)
