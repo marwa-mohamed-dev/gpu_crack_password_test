8b9630d1d0077ee404c6fba764f53b0da18616d2
marwa-mohamed-dev <47494409+marwa-mohamed-dev@users.noreply.github.com>
2023-03-11 21:02:47 +0100

    Update gpu.py

--- gpu.py
+++ gpu.py
@@ -1,8 +1,8 @@
-from numba import cuda
+from numba import cuda, njit
 import brute_force as b
 import hashlib
 
-@cuda.jit
+@njit
 def kernel_combinaisons(mdp, hash):
     all = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','@','[',']','^','_','!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','{','}','<','>','=','|','~','?']
     tx = cuda.threadIdx.x
